e14e436fc3819796d60fad9c7e9ec578caab3c91
pedroquirino <pedrohpquirino@gmail.com>
2026-09-20 23:41:46 -0300

    feat(T1.4): nova tela HomeView

--- georuraldatahub_front/src/views/HomeView.vue
+++ georuraldatahub_front/src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 class="text-h5 mb-4">Início</h1>
+  <p class="mb-4">Esta é a próxima tela do projeto.</p>
+  <v-btn to="/">Voltar ao login</v-btn>
 </template>
