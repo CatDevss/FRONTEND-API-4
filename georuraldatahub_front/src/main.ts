d76b9d9068282da33aa9cbfecc5505de84df15a7
pedroquirino <pedrohpquirino@gmail.com>
2026-09-20 22:46:50 -0300

    feat: realizado configuracao do vuetify

--- georuraldatahub_front/src/main.ts
+++ georuraldatahub_front/src/main.ts
@@ -2,7 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import vuetify from './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 
